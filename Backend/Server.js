26010c3d229498e3de6f90ab2be0835353264c20
Alishba shekh <alishbashabbir890gmail.com>
2026-02-06 00:41:59 -0800

    ..

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,6 +4,7 @@ import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
@@ -48,6 +49,6 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is responding");
 });
